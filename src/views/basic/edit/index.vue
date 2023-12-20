ee070bc3b05dcbfdf5464eedf904f22b83ae7600
xdd2123 <21236774@qq.com>
2023-12-20 17:59:16 +0800

    update: 更新文字描述信息

--- src/views/basic/edit/index.vue
+++ src/views/basic/edit/index.vue
@@ -4,7 +4,7 @@ import Drager from '@/components/Drager/index.vue'
 
 <template>
   <div class="w-full h-400 bg-bg-color rounded-2xl">
-    <Drager :boundary="true" />
+    <Drager :boundary="true"> 点我拖拽 </Drager>
   </div>
 </template>
 
