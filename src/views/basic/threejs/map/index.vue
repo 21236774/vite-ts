d66b7c32b722912f9a24964de637857a249354ca
xdd2123 <21236774@qq.com>
2024-07-05 12:18:42 +0800

    update: threejs地图组件

--- src/views/basic/threejs/map/index.vue
+++ src/views/basic/threejs/map/index.vue
@@ -1,41 +1,24 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup>
 import * as THREE from 'three'
-import { ref, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import * as d3 from 'd3'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-/**
- * 获取网格的包围盒
- * @param {Object3D} group 网格对象
- * @returns
- */
-function getBoundBox(group) {
-  // 计算实际宽高
-  var size = new THREE.Vector3()
+import {
+  lineDraw,
+  getBoundBox,
+  calcUv2,
+  controlsFn,
+  createLabel,
+  createCss3dRender
+} from '@/utils/threejs'
+import { drawExtrudeMesh, addEventMap, setPointAndTip } from './hooks'
+import { circleAt } from './hooks/animation'
+import { InteractionManager } from 'three.interactive'
 
-  // 包围盒计算模型对象的大小和位置
-  var box3 = new THREE.Box3()
-  box3.expandByObject(group) // 计算模型包围盒
-  var boxSize = new THREE.Vector3()
-  box3.getSize(boxSize) // 计算包围盒尺寸
-  var center = new THREE.Vector3()
-  box3.getCenter(center) // 计算一个层级模型对应包围盒的几何体中心坐标
-  let obj = {
-    box3,
-    boxSize,
-    center
-  }
-  if (group.geometry) {
-    group.geometry.computeBoundingBox()
-    group.geometry.computeBoundingSphere()
-    const { max, min } = group.geometry.boundingBox
-    size.x = max.x - min.x
-    size.y = max.y - min.y
-    size.z = max.z - min.z
-    obj.size = size
-  }
-  return obj
-}
+const defaultColor = '#F69F47'
+const activeColor = '#F9C978'
+const d3Center = [112.271301, 30.987527]
+
 const props = defineProps({
   id: {
     type: [Number, String],
@@ -53,19 +36,10 @@ const props = defineProps({
 
 const scene = new THREE.Scene() // 创建场景
 let camera = null // 创建相机
+let addPointFn = null
+let removePointFn = null
 const renderer = new THREE.WebGLRenderer({ antialias: true }) // 创建渲染器
-function animate() {
-  requestAnimationFrame(animate)
-  // 如果有动画，更新相关对象的位置、旋转等属性
-  renderer.render(scene, camera)
-}
-const objFn = () => {
-  const controls = new OrbitControls(camera, renderer.domElement)
-  controls.addEventListener('change', function () {
-    renderer.render(scene, camera) //执行渲染操作
-  })
-}
-
+renderer.setClearAlpha(0)
 // 设置点光源
 const setPointLight = (scene) => {
   let sun = new THREE.AmbientLight(0xffffff, 2)
@@ -80,234 +54,182 @@ const setPointLight = (scene) => {
   scene.add(directionalLight)
 }
 
-// 设置Uv贴图
-const calcUv2 = (geometry, width, height, minX, minY) => {
-  const positionAttribute = geometry.attributes.position
-  const uvAttribute = geometry.attributes.uv
-
-  const count = geometry.groups[0].count
-  for (let i = 0; i < count; i++) {
-    const x = positionAttribute.getX(i)
-    const y = positionAttribute.getY(i)
-
-    const u = (x - minX) / width
-    const v = (y - minY) / height
-
-    uvAttribute.setXY(i, u, v)
-  }
-
-  uvAttribute.needsUpdate = true
-  geometry.computeVertexNormals()
-}
-
 onMounted(() => {
-  const container = document.getElementById('threejsSquare' + props.id)
-  scene.background = new THREE.Color(0xbfe3dd)
+  const container = document.getElementById('canvas')
+  const css3dRender = createCss3dRender(container)
+  // scene.background = new THREE.Color(0xbfe3dd)
   camera = new THREE.PerspectiveCamera(
-    40,
+    10,
     container?.offsetWidth / container?.offsetHeight || 0,
-    0.1,
-    10000
+    10,
+    1000
   )
-  camera.position.set(0, 0, 300) // 举例，设置相机的位置
-  camera.lookAt(0, 0, 20)
-  const axes = new THREE.AxesHelper(700)
-  scene.add(axes)
+  camera.position.set(0, -50, 70) // 举例，设置相机的位置
+  camera.lookAt(0, 0, 0)
+  // const axes = new THREE.AxesHelper(700)
+  // scene.add(axes)
+  const interactionManager = new InteractionManager(
+    renderer,
+    camera,
+    renderer.domElement
+  )
   setPointLight(scene)
+  circleAt(scene)
 
   // 请求湖北数据
   const loader = new THREE.FileLoader()
   loader.load(
-    'https://geo.datav.aliyun.com/areas_v3/bound/100000_full.json',
+    'https://geo.datav.aliyun.com/areas_v3/bound/420000_full.json',
     (data) => {
       const jsondata = JSON.parse(data)
       operationData(jsondata)
     }
   )
-  const textureLoader = new THREE.TextureLoader()
-  const getTextureMash = () => {
-    const topNormal = textureLoader.load('img/ganyu.png')
-    topNormal.wrapS = topNormal.wrapT = THREE.RepeatWrapping
-    const sideTexture = textureLoader.load('img/side.png')
-    sideTexture.wrapS = sideTexture.wrapT = THREE.RepeatWrapping
-    console.log(sideTexture, 222)
-    const topMaterial = new THREE.MeshStandardMaterial({
-      color: '#e5e7eb',
-      emissive: 0x000000,
-      map: topNormal,
-      transparent: true,
-      normalMap: topNormal,
-      opacity: 1
-    })
-    sideTexture.repeat.set(1, 0.2)
-    sideTexture.offset.y += 0.01
-    const sideMaterial = new THREE.MeshStandardMaterial({
-      // color: 0x62c3d1,
-      color: 0xffffff,
-      map: sideTexture,
-      fog: false,
-      opacity: 0
-    })
-    sideMaterial.onBeforeCompile = (shader) => {
-      shader.uniforms = {
-        ...shader.uniforms,
-        uColor1: { value: new THREE.Color(0x30b3ff) },
-        uColor2: { value: new THREE.Color(0x30b3ff) }
-      }
-      shader.vertexShader = shader.vertexShader.replace(
-        'void main() {',
-        `
-        attribute float alpha;
-        varying vec3 vPosition;
-        varying float vAlpha;
-        void main() {
-          vAlpha = alpha;
-          vPosition = position;
-      `
-      )
-      shader.fragmentShader = shader.fragmentShader.replace(
-        'void main() {',
-        `
-        varying vec3 vPosition;
-        varying float vAlpha;
-        uniform vec3 uColor1;
-        uniform vec3 uColor2;
-      
-        void main() {
-      `
-      )
-      shader.fragmentShader = shader.fragmentShader.replace(
-        '#include <opaque_fragment>',
-        /* glsl */ `
-      #ifdef OPAQUE
-      diffuseColor.a = 1.0;
-      #endif
-      
-      // https://github.com/mrdoob/three.js/pull/22425
-      #ifdef USE_TRANSMISSION
-      diffuseColor.a *= transmissionAlpha + 0.1;
-      #endif
-      vec3 gradient = mix(uColor1, uColor2, vPosition.z/1.2);
-      
-      outgoingLight = outgoingLight*gradient;
-      
-      
-      gl_FragColor = vec4( outgoingLight, diffuseColor.a  );
-      `
-      )
-    }
-    return [topMaterial, sideMaterial]
-  }
 
-  const projection = d3
-    .geoMercator()
-    .center([116.412318, 39.909843])
-    .translate([0, 0]) // 坐标转换，设置北京中心点
-  // 绘制面，材质
-  function drawExtrudeMesh(polygon, color, info) {
-    const shape = new THREE.Shape()
-    polygon.forEach((row, i) => {
-      const [x, y] = projection(row)
-      if (i === 0) {
-        shape.moveTo(x, y)
-      }
-      shape.lineTo(x, y)
-    })
-    console.log(polygon)
-    // 拉伸
-    const geometry = new THREE.ExtrudeGeometry(shape, {
-      depth: 1,
-      bevelEnabled: true,
-      bevelSegments: 1,
-      bevelThickness: 0.1
-    })
-    const materials = getTextureMash()
-    const mesh = new THREE.Mesh(geometry, materials)
-    mesh.userData = info
-    return mesh
-  }
-
+  const projection = d3.geoMercator().center(d3Center).translate([0, 0]) // 坐标转换，设置湖北中心点
   const map = new THREE.Group()
+
   // 解析数据
   function operationData(jsondata) {
     // 全国信息
     const features = jsondata.features
+    const regionList = [] // 区域数据
     features.forEach((feature) => {
       // 单个省份
+      let { name, centroid = [], adcode } = feature.properties
       const group = new THREE.Group()
+      regionList.push(feature.properties)
+      group.uuid = 'group-' + adcode
       const groupLine = new THREE.Group()
+      const [x, y] = d3
+        .geoMercator()
+        .center(d3Center)
+        .scale(150)
+        .translate([-1.2, -0.2])(centroid)
+      // label 市文字
+      const nameLabel = createLabel(
+        `<span>${name}</span><span class="hidden-text-label">${name}</span>`,
+        'map-label',
+        new THREE.Vector3(x, -y, 1.1)
+      )
       // 地址
       const coordinates = feature.geometry
       group.userData = feature.properties // 存储当前数据
-      const color = 'red'
       if (feature.geometry.type === 'MultiPolygon') {
         // 多个，多边形
         coordinates.coordinates.forEach((coordinate) => {
           // coordinate 多边形数据
           coordinate.forEach((rows) => {
-            const mesh = drawExtrudeMesh(rows, color, feature.properties)
-            const line = lineDraw(rows, 0x62c3d1)
+            const mesh = drawExtrudeMesh(
+              rows,
+              feature.properties,
+              projection,
+              defaultColor
+            )
+            const line = lineDraw(rows, '#FDECA0', projection)
             groupLine.add(line)
             group.add(mesh)
           })
         })
       }
-
-      if (feature.geometry.type === 'Polygon') {
-        // 多边形
-        coordinates.coordinates.forEach((coordinate) => {
-          const mesh = drawExtrudeMesh(coordinate, color)
-          group.add(mesh)
-        })
-      }
-      map.add(groupLine)
+      // map.add(groupLine)
+      group.add(nameLabel)
+      group.add(groupLine) // 全部添加一组执行，动画就是一起的
       map.add(group)
     })
     scene.add(map)
+    const { addPointTip, removePointTip } = setPointAndTip(
+      scene,
+      interactionManager,
+      regionList,
+      d3Center
+    )
+    addPointFn = addPointTip
+    removePointFn = removePointTip
+    // scene.add(pointArr)
+    // 先隐藏，不然默认都显示
 
-    // 为每组纹理贴图UV
+    setTimeout(() => {
+      const arr = document.getElementsByClassName('map-label') || []
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].style.visibility = 'visible'
+      }
+      // addPointTip()
+    }, 1000)
+
+    // 处理整个背景贴图
     const { boxSize, box3 } = getBoundBox(map)
+    const elementData = []
     map.children.forEach((group) => {
       group.children.map((mesh) => {
         if (mesh.type === 'Mesh') {
+          elementData.push(mesh)
           calcUv2(mesh.geometry, boxSize.x, boxSize.y, box3.min.x, box3.min.y)
         }
       })
     })
-    map.rotation.x = Math.PI
-    animate()
+    addEventMap(elementData, activeColor, map, interactionManager)
+    animate(interactionManager)
   }
-
-  /**
-   * 边框 图形绘制
-   * @param polygon 多边形 点数组
-   * @param color 材质颜色
-   * */
-  function lineDraw(polygon, color) {
-    const lineGeometry = new THREE.BufferGeometry()
-    // eslint-disable-next-line @typescript-eslint/no-array-constructor
-    const pointsArray = new Array()
-    polygon.forEach((row) => {
-      const [x, y] = projection(row)
-      // 创建三维点
-      pointsArray.push(new THREE.Vector3(x, y, 1.12))
-    })
-    // 放入多个点
-    lineGeometry.setFromPoints(pointsArray)
-
-    const lineMaterial = new THREE.LineBasicMaterial({
-      color: color
-    })
-    return new THREE.Line(lineGeometry, lineMaterial)
+  function animate() {
+    requestAnimationFrame(animate)
+    interactionManager && interactionManager.update()
+    css3dRender.render(scene, camera)
+    renderer.render(scene, camera)
   }
-
   renderer.setSize(container.clientWidth, container.clientHeight)
   container.appendChild(renderer.domElement)
-  objFn()
+  controlsFn(camera, scene, renderer)
 })
+const addPoint = (flag) => {
+  if (flag) addPointFn()
+  else removePointFn()
+}
 </script>
 <template>
-  <div style="width: 100%; height: 800px">
-    <div :id="'threejsSquare' + id" class="w-full h-full"></div>
+  <div class="flex" style="width: 100%; height: 800px">
+    <div id="canvas" class="w-full h-full relative"></div>
+    <button @click="addPoint(1)">添加落点</button>
+    <button @click="addPoint(0)">删除落点</button>
   </div>
 </template>
+
+<style lang="scss">
+.map-label {
+  color: #fff;
+  font-size: 16px;
+  background: #ff013c;
+  white-space: nowrap;
+  border: 0;
+  color: #fff;
+  position: relative;
+  z-index: 22;
+  span {
+    position: absolute;
+    z-index: 2;
+    background: #ff013c;
+    -webkit-clip-path: polygon(
+      100% 0%,
+      100% 51%,
+      100% 100%,
+      12% 100%,
+      0 59%,
+      0 1%
+    );
+    padding: 4px 8px;
+    clip-path: polygon(100% 0%, 100% 51%, 100% 100%, 12% 100%, 0 59%, 0 1%);
+    // &:after {
+    //   position: absolute;
+    //   height: calc(100% + 6px);
+    //   content: '';
+    //   width: 100%;
+    // }
+  }
+  .hidden-text-label {
+    background: #f5eda5;
+    z-index: -1;
+    top: 6px;
+    padding-right: 14px;
+  }
+}
+</style>
