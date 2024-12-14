cdc698109bfce493a0769490d265f27f1f87ab6b
21236774 <58690897+21236774@users.noreply.github.com>
2024-12-14 11:06:04 +0800

    Update user.ts
    环境变量

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,21 +1,22 @@
 import { request } from '@/service'
 
+const baseUrl = import.meta.env.VITE_API_URL
 export const getRoutes = (): Promise<ApiData<AuthRoute.Route[]>> => {
-  return request.get('/api-proxy/users/router')
+  return request.get(baseUrl + '/users/router')
 }
 // 获取用户信息
 export const getUserInfo = (): Promise<ApiData<User.UserApiInfo>> => {
-  return request.get('/api-proxy/users/user-info')
+  return request.get(baseUrl + '/users/user-info')
 }
 
 // 登录
 export const loginApi = (
   info: User.UserInfo
 ): Promise<ApiData<User.UserApiInfo>> => {
-  return request.post('/api-proxy/users/login', { ...info })
+  return request.post(baseUrl + '/users/login', { ...info })
 }
 
 // 退出登录
 export const logoutApi = () => {
-  return request.get('/api-proxy/users/logout')
+  return request.get(baseUrl + '/users/logout')
 }
