f146692207f8b563783a1940c4731092d8aac318
21236774 <58690897+21236774@users.noreply.github.com>
2024-12-14 11:12:47 +0800

    Update article.ts
    环境变量

--- src/api/article.ts
+++ src/api/article.ts
@@ -1,17 +1,18 @@
 import { request } from '@/service'
 
+const baseUrl = import.meta.env.VITE_API_URL
 export const getArticle = (): any => {
-  return request.get('/api-proxy/article/all-list')
+  return request.get(baseUrl + '/article/all-list')
 }
 
 export const getUserArticle = (params: any): any => {
-  return request.post('/api-proxy/article/user-list', { ...params })
+  return request.post(baseUrl + '/article/user-list', { ...params })
 }
 
 export const updateArticle = (params: any): any => {
-  return request.post('/api-proxy/article/new', { ...params })
+  return request.post(baseUrl + '/article/new', { ...params })
 }
 
 export const delArticle = (id: string): any => {
-  return request.get('/api-proxy/article/del?id=' + id)
+  return request.get(baseUrl + '/article/del?id=' + id)
 }
